0aa94da329e54059c32624df1fc26412765fbb67
American Study Dev <retdkhoa@gmail.com>
2026-01-25 10:54:17 +0700

    [f] Canh + Tammy Update UI
    Co-authored-by: Tammy N <shrikeo@users.noreply.github.com>
    Co-authored-by: QC09 <QC09@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 from typing import Union
 from fastapi.responses import HTMLResponse, JSONResponse
 import json
-
-from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Request
+from service import googleAIClient
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/task")
+def read_task_page(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="today.html",
+    )
+
+# @app.get('/')
+# def read_root(request: Request):
+#     return templates.TemplateResponse(
+#         request=request,
+#         name="index.html",
+#         context={"content": "Your content here", "text": "OK"}
+#     )
+
 @app.get("/analyze")
-def analyze(task):
-    
-    return {"title": "essay", "estimated_time": "7 hours", "risk": "high effort task"}
+def analyze_task(q: str):
+    response = googleAIClient.generate_response(q)
+
+    return response
--- service.py
+++ service.py
@@ -1,24 +1,41 @@
+from typing import List, Optional
 from google import genai
 from dotenv import load_dotenv
+from pydantic import BaseModel, Field
 load_dotenv()
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
 client = genai.Client()
 
 GOOGLE_INSTRUCTION = """You must answer in 1 setence starting with "Hello Canh and Tammy". You are an very smart assistant. Your task is to analyze my current tasks and deadlines so that you can help to review this with following criterias:
 
-    - Based on my currents task
+    - Based on my current assignment, help me to analyze as my task based on the following criterias:
+        - My deadline
+        - My effort will be estimate based on sclae from 1-10, for the easy to hard.
+    - And help me to suggest my task based on this assignments.
 """
+
+class Task(BaseModel):
+    name: str
+    description: str
+
+class Summary(BaseModel):
+    name: str = Field(description="The name of the recipe.")
+    duedate: Optional[str] = Field(description="Optional time in minutes to prepare the recipe.")
+    tasks: List[Task]
+    effort: int
 
 class GoogleAIService:
     def generate_response(self, input):
         response = client.models.generate_content(
             model= "gemini-2.5-flash", contents = input,
             config = {
-                "system_instruction": GOOGLE_INSTRUCTION
+                "system_instruction": GOOGLE_INSTRUCTION,
+                "response_mime_type": "application/json",
+                "response_json_schema": Summary.model_json_schema(),
             }
         )
         return response
 
 googleAIClient = GoogleAIService()
-response = googleAIClient.generate_response("help me to analyse this task: ")
-print(response.text)
+# response = googleAIClient.generate_response("My assigment is Economics project, deadline is 2 week from now, I need to create a sample project on micro-economics.")
+# print(response.text)
